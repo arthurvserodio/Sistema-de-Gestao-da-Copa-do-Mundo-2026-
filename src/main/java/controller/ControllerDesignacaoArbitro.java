b23734b0cf42f26b1ed677c35130b938401555f0
Gustavo Pires <gmsgu89@gmail.com>
2026-06-01 09:52:08 -0300

    feat: Iniciando parte de designação dos arbitros

---
+++ src/main/java/controller/ControllerDesignacaoArbitro.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+
+import javax.swing.text.html.ImageView;
+
+public class ControllerDesignacaoArbitro {
+
+    @FXML
+
+    @FXML
+    public void initialize(){
+
+    }
+    
+}
